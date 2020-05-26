f674aa4271c2ef44454f35addfa9ab2e2223b488
Kevin Sattakun <42414822+kevsatt@users.noreply.github.com>
2020-05-25 17:15:48 -0700

    Add files via upload

--- troll_bot.py
+++ troll_bot.py
@@ -48,15 +48,15 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, current_tweet):
         """ upon the user tweeting, this function will run and find the subject of the tweet and reply with {subject}
-        is for squares! What a zinger. spicy. """
+        IS FOR SQUARES! What a zinger. spicy. """
         if from_creator(current_tweet):
             # filter to make the subject not be any of these words
             filter_ = ['I', 'i', 'it', ' IT', 'It']
             doc = nlp(current_tweet.text)
             subjects = [chunk.text.upper() for chunk in doc.noun_chunks if chunk.text not in filter_]
 
-            # have a blanket best subject be 'YOU' in the case that the subject returns empty due to the filter
-            best_subject = 'YOU'
+            # have a blanket best subject be 'SMOKING' in the case that the subject returns empty due to the filter
+            best_subject = 'SMOKING'
             max_len_subject = -1
             for subject in subjects:
                 if '@' not in subject:
@@ -65,10 +65,10 @@ class MyStreamListener(tweepy.StreamListener):
                         best_subject = subject
 
             # reply is sent here
-            tweet_to_send = f'@{current_tweet.user.screen_name} {best_subject} is for squares!'
+            tweet_to_send = f'@{current_tweet.user.screen_name} {best_subject} IS FOR SQUARES!'
             api.update_status(tweet_to_send,
                               in_reply_to_status_id=current_tweet.id)
-            message = f'NutsOnChinbot tweeted: {tweet_to_send}'
+            message = f'Troll Bot tweeted: {tweet_to_send}'
             print(message)
             send_text(message)
             return message
@@ -77,7 +77,7 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_error(self, status):
         """ records whenever an error occurs then calls send_text function to send me a text message """
-        message = f'Error detected on NutsOnChinBot: {status}'
+        message = f'Error detected on Troll Bot: {status}'
         print(message)
         send_text(message)
         return message
